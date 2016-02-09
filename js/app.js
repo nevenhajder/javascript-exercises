7ad437043faeccd8fc74158adb81ba33529f58cc
Neven Hajder <neven.hajder@gmail.com>
2016-02-09 12:14:30 -0500

    Filter array for larger than num

--- js/app.js
+++ js/app.js
@@ -1,54 +1,25 @@
 
 /*  Assignment:
-        Write a JavaScript function to  get the number of occurrences of each letter in specified string.
-        eg. neven -> n = 2, v = 1, e = 2
+        Write a JavaScript function that returns array elements larger than a number.
+
 */
 
 (function () {
     "use strict";
 
-    /* Variables */
-    var userInput = document.getElementById('userInput');
-    var btnCount = document.getElementById('btnCount');
-    var display = document.getElementById('display');
+    var testArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 45, 50, 13, 18];
+    var testNum = 15;
+    var largerList = [];
 
-    /* Functions */
-    function countChars(str) {
-        var regex;
-        var count;
-        var results = [];
+    function largerThan(n, arr) {
+        largerList = arr.filter((el) => {
+            return el > n;
+        });
 
-        while (str.length > 0) {
-            /* Create a new RegExp with the first letter in str */
-            regex = new RegExp(str.charAt(0), 'g');
-
-            /* Search the string using regex */
-            count = str.match(regex).length;
-
-            /* Push the character and its count to the results array */
-            results.push('\n'+str.charAt(0)+' = '+count);
-
-            /* Remove the counted character in the string */
-            str = str.replace(regex, '');
-        }
-
-        return results;
+        return largerList;
     }
 
-    function displayResults(arg) {
-        display.innerText = arg;
-    }
-
-    /* Events */
-    btnCount.addEventListener('click', () => {
-        var userString = userInput.value;
-        displayResults(countChars(userString));
-    });
-
-
-
-
-    countChars('thtequickbrownfxjmpsvlazydg');
+    console.log(largerThan(testNum, testArray));
 
 }());
 
