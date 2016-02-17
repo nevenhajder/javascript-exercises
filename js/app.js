ddaac9594a8e6c6f07665458cd76ffccfd3c5828
Neven Hajder <neven.hajder@gmail.com>
2016-02-17 09:51:54 -0500

    Number guessing game - Rebecca Murphey

--- js/app.js
+++ js/app.js
@@ -1,34 +1,65 @@
 
 /*  Assignment:
-        Write a JavaScript function that generates a string id (specified length) of random characters.
-        Sample: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 
+- Write the `init` function to set up an event listener on the form. The event listener should pass the value of the input element to the `check` function.
+- Write the `check` function to accept a value from the event listener and check it against the `targetNumber`.
+  - If the values match, call the `showWin` function
+  - If the values do not match, call the `showError` function.
+  - If the values do not match, and the player has made more than five guesses, call the `showLoss` function.
+- Write the `showWin` function to remove the form and any error message, and show a message telling the player they win.
+- Write the `showError` function to show a message telling the player their guess is incorrect.
+- Write the `showLoss` function to remove the form and show a message telling the player they lose.
 */
 
 (function () {
     "use strict";
 
-    var btnID = document.getElementById('btnID');
-    var display = document.getElementById('display');
-    var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    var form = document.getElementsByTagName('form')[0];
+    var targetNumber = Math.floor(Math.random() * 10) + 1;
+    var guesses = 0;
 
-    function generateID(l) {
-        var newID = '';
-        for (var i = 0; i<l; i++) {
-            newID += characters.charAt(Math.floor(Math.random()*characters.length));
+    function init () {
+        form.addEventListener('submit', function(e) {
+            /* Prevent form submission */
+            e.preventDefault();
+            guesses++;
+            var userNum = parseInt(document.getElementsByTagName('input')[0].value,10);
+            check(userNum);
+        });
+    }
+
+    function check (value) {
+        console.log('Guess: '+value);
+        console.log('Guesses: '+guesses);
+
+        if (targetNumber !== value && guesses > 5) {
+            showLoss();
+        } else if (targetNumber === value) {
+            showWin();
+        } else {
+            showError();
         }
-        return newID;
     }
 
-    function displayResult(arg) {
-        display.innerText = arg;
+    function showWin () {
+        /* Remove the form */
+        form.parentNode.removeChild(form);
+        alert('You win!');
     }
 
-    btnID.addEventListener('click', () => {
-        displayResult(generateID(8));
-    });
+    function showError () {
+        alert('You guessed wrong :(');
+    }
 
-}());
+    function showLoss () {
+        /* Remove the form */
+        form.parentNode.removeChild(form);
+        alert('You lose ... ');
+    }
+
+    init();
+
+})();
 
 
 
